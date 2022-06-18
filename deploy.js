31171874b81b21d0fbe7cee0095d6207b8439c1f
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-17 23:10:38 -0500

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -15,10 +15,6 @@ async function main() {
 
     const contract = await contractFactory.deploy();
     const deploymentReceipt = await contract.deployTransaction.wait(1);
-    console.log("Here is the deployment transaction (transaction response): ");
-    console.log(contract.deployTransaction);
-    console.log("Here is the transaction receipt: ");
-    console.log(deploymentReceipt);
 }
 
 main()
