37cc6ba5659ad468a414e99566039172a5327aa4
Ángel Sánchez <mektigboy@protonmail.com>
2022-06-18 21:04:53 -0500

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,12 +1,11 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
 
+require("dotenv").config();
+
 async function main() {
     const provider = new ethers.providers.JsonRpcProvider("http://0.0.0.0:8545"); // Connect to local blockchain.
-    const wallet = new ethers.Wallet(
-        "4a083bf5e32f3a230980d9beb22ec5abd72317659ed4650552ab6e47b274bd73",
-        provider
-    );
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
