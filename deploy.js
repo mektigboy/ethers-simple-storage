de603e275e036334a870c7da0bcc2b4bc294917f
mektigboy <mektigboy@protonmail.com>
2022-07-27 11:59:51 -0500

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -6,20 +6,26 @@ require("dotenv").config();
 async function main() {
     const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL); // Connect to local blockchain.
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-    const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
-    const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8");
+    const abi = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.abi",
+        "utf8"
+    );
+    const binary = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.bin",
+        "utf8"
+    );
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
 
     console.log("Deploying. Please wait...");
 
     const contract = await contractFactory.deploy();
-    
+
     await contract.deployTransaction.wait(1);
 
     console.log(`Contract address: ${contract.address}`);
-    
+
     const currentFavoriteNumber = await contract.retrieve();
-    
+
     console.log(`Current favorite number: ${currentFavoriteNumber.toString()}`);
 
     const transactionResponse = await contract.store("7");
